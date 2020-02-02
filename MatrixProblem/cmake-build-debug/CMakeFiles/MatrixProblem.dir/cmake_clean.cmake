3774dd6a8ff5ced4f11e602fa9d46a9701a93851
TheRidd <cristian.braslasu@gmail.com>
2020-02-02 15:00:26 +0200

    MatrixProblem

---
+++ MatrixProblem/cmake-build-debug/CMakeFiles/MatrixProblem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatrixProblem.dir/main.cpp.obj"
+  "MatrixProblem.exe"
+  "MatrixProblem.exe.manifest"
+  "MatrixProblem.pdb"
+  "libMatrixProblem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatrixProblem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
